8d19f7f36bca28ffa31d23ae19ef4b8bccbc96a0
root <1Eba>
2022-03-12 13:10:22 -0800

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-#Creates a file in /tmp
-file{
-        '/tmp/holberton':
-        mode    => '0744',
-        owner   => 'www-data',
-        group   => 'www-data',
-        content => 'I love Puppet',
+# Puppet creates a file in /tmp
+
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
